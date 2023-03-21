fc25f0c8f3387c2307faec0c29280b3ca37eb44b
KYapp <kukiyomenaisapp@gmail.com>
2023-03-21 18:01:51 +0900

    PlayerをGameManagerが参照するように

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,10 @@
+using AnnulusGames.LucidTools.Inspector;
 using JuhaKurisu.PopoTools.Utility;
 using UnityEngine;
 
 [HideMonoScript]
 public class GameManager : Singleton<GameManager>
 {
-
+    [SerializeField, Required] private Player _player;
+    public Player player => _player;
 }
