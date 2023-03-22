79b63234e1d534d9b063ef314ff0e6f6a644b276
KYapp <kukiyomenaisapp@gmail.com>
2023-03-22 22:53:37 +0900

    Map

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,54 @@
+using UniRx;
+
+public class Map
+{
+    public readonly Subject<TilePosition> onTileDestroyed;
+    public readonly Subject<TilePosition> onTileTakeDamaged;
+    public readonly Subject<TilePosition> onTileUpdated;
+    private Tile[,] tiles;
+
+    public Map()
+    {
+        tiles = new Tile[10, 10];
+        onTileDestroyed = new();
+        onTileTakeDamaged = new();
+        onTileUpdated = new();
+    }
+
+    public Tile GetTile(TilePosition tilePosition)
+    {
+        return tiles[tilePosition.x, tilePosition.y];
+    }
+
+    public bool TryAddDamage(int power, TilePosition tilePosition)
+    {
+        Tile tile = tiles[tilePosition.x, tilePosition.y];
+
+        // 破壊不可能の場合はそもそも破壊しない
+        if (!Settings.tileContents[(int)tile.id].destructible) return false;
+
+        TileID id = tile.id;
+        TileDamage damage = new(tile.damage.value + power);
+
+        // ダメージが許容値を超えた場合破壊する
+        if (Settings.tileContents[(int)id].durability <= damage.value)
+        {
+            // 破壊されたことの通知
+            onTileDestroyed.OnNext(tilePosition);
+
+            id = TileID.Empty;
+            damage = new();
+        }
+
+        // セット
+        tiles[tilePosition.x, tilePosition.y] = new Tile(id, damage);
+
+        //ダメージを受けたことの通知
+        onTileTakeDamaged.OnNext(tilePosition);
+
+        // 更新
+        onTileUpdated.OnNext(tilePosition);
+
+        return true;
+    }
+}
