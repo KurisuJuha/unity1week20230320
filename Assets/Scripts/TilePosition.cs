571e51e7e8abdae7befe60ee4aac7e49f4c7299a
KYapp <kukiyomenaisapp@gmail.com>
2023-03-22 22:52:46 +0900

    TilePosition

---
+++ Assets/Scripts/TilePosition.cs
@@ -0,0 +1,11 @@
+public struct TilePosition
+{
+    public readonly int x;
+    public readonly int y;
+
+    public TilePosition(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
