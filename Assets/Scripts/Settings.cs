f3a1b1d403e07725b90afb80a1f28de6822c4d6a
KYapp <kukiyomenaisapp@gmail.com>
2023-03-22 22:53:31 +0900

    SettingsにtileContentsを追加した

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,6 +1,9 @@
+using System.Collections.ObjectModel;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class Settings : ScriptableObject
 {
+    [SerializeField] private TileContents[] _tileContentsList;
+    public static ReadOnlyCollection<TileContents> tileContents => new(GameManager.Instance.settings._tileContentsList);
 }
